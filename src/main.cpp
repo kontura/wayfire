22d2842da703aeb3b8f1b16a2d11629bcd38d88b
Aleš Matěj <amatej@redhat.com>
2020-08-01 08:38:14 +0200

    weird xml dir for plugins fail

--- src/main.cpp
+++ src/main.cpp
@@ -358,7 +358,7 @@ int main(int argc, char *argv[])
 
     LOGI("using config file: ", config_file.c_str());
     core.config = wf::config::build_configuration(
-        xmldirs, SYSCONFDIR "/wayfire/defaults.ini", config_file);
+        PLUGIN_XML_DIR, SYSCONFDIR "/wayfire/defaults.ini", config_file);
 
     int inotify_fd = inotify_init1(IN_CLOEXEC);
     reload_config(inotify_fd);
